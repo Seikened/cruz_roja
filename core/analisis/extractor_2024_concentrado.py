faf59dd8e637ca2f982d968be10c39ce5eb51103
Fernando Leon Franco <fernanlee2131@gmail.com>
2026-01-31 00:18:42 -0600

    feat: agregar cambios en la lógica de negocio
    - Se implementaron mejoras en la validación de datos.
    - Se optimizó el rendimiento de las consultas a la base de datos.
    - Se corrigieron errores menores en la interfaz de usuario.

--- core/analisis/extractor_2024_concentrado.py
+++ core/analisis/extractor_2024_concentrado.py
@@ -1,5 +1,6 @@
 import polars as pl
 from pathlib import Path
+from colorstreak import Logger as log
 
 base_dir = Path(__file__).resolve().parent
 año = "2024"
@@ -9,8 +10,18 @@ archivos_a_leer = [base_dir / "data" / año / f"bitacora_{año}_{mes}.csv" for m
 archivo_salida = base_dir / "data" / año / f"bitacora_{año}_concentrado.csv"
 
 
-lista_df = [pl.read_csv(archivo) for archivo in archivos_a_leer]
+lista_df = [pl.read_csv(archivo,infer_schema_length=0) for archivo in archivos_a_leer]
 
 
+filas_totales = sum(df.height for df in lista_df)
+log.info(f"Cantidad total de filas recuperadas: {filas_totales}")
 for df in lista_df:
-    print(f"Columnas en el archivo: {df.columns}")
+    
+    log.info(f"Columnas: {df.columns}")
+
+
+df_final = pl.concat(lista_df, how="vertical")
+df_final.write_csv(archivo_salida)
+cantidad_filas = df_final.height
+log.success(f"Cantidad de filas en el DataFrame final: {cantidad_filas}")
+log.debug(f"Primeras filas del DataFrame: \n{df_final}")
